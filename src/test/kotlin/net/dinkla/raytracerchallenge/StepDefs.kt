ac2f223a9d93ab8a7e91ead8d5f059d0f307a9a6
Jörn Dinkla <joern@dinkla.net>
2020-12-28 17:42:26 +0100

    Add features: tuple

--- src/test/kotlin/net/dinkla/raytracerchallenge/StepDefs.kt
+++ src/test/kotlin/net/dinkla/raytracerchallenge/StepDefs.kt
@@ -4,12 +4,16 @@ import io.cucumber.java.PendingException
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.When
 import io.cucumber.java.en.Then
+import net.dinkla.raytracerchallenge.math.point
 import net.dinkla.raytracerchallenge.math.tuple
+import net.dinkla.raytracerchallenge.math.vector
 import org.junit.Assert.*
 
 class StepDefs {
 
     lateinit var a: tuple
+    lateinit var p: tuple
+    lateinit var v: tuple
 
     @Given("a ← tuple\\({double}, {double}, {double}, {double})")
     fun a_tuple(double1: Double?, double2: Double?, double3: Double?, double4: Double?) {
@@ -44,5 +48,35 @@ class StepDefs {
     @Then("a is not a vector")
     fun a_is_not_a_vector() {
         assertFalse(a.isVector())
+    }
+
+    @Then("a is not a point")
+    fun a_is_not_a_point() {
+        assertFalse(a.isPoint())
+    }
+
+    @Then("a is a vector")
+    fun a_is_a_vector() {
+        assertTrue(a.isVector())
+    }
+
+    @Given("p ← point\\({int}, {int}, {int})")
+    fun p_point(int1: Int?, int2: Int?, int3: Int?) {
+        p = point(int1!!, int2!!, int3!!)
+    }
+
+    @Then("p = tuple\\({int}, {int}, {int}, {int})")
+    fun p_tuple(int1: Int?, int2: Int?, int3: Int?, int4: Int?) {
+        assertEquals(p, tuple(int1!!, int2!!, int3!!, int4!!))
+    }
+
+    @Given("v ← vector\\({int}, {int}, {int})")
+    fun v_vector(int1: Int?, int2: Int?, int3: Int?) {
+        v = vector(int1!!, int2!!, int3!!)
+    }
+
+    @Then("v = tuple\\({int}, {int}, {int}, {int})")
+    fun v_tuple(int1: Int?, int2: Int?, int3: Int?, int4: Int?) {
+        assertEquals(v, tuple(int1!!, int2!!, int3!!, int4!!))
     }
 }
--- src/test/resources/tuples.feature
+++ src/test/resources/tuples.feature
@@ -9,23 +9,23 @@ Scenario: A tuple with w=1.0 is a point
     And a is a point
     And a is not a vector
 
-#Scenario: A tuple with w=0 is a vector
-#  Given a ← tuple(4.3, -4.2, 3.1, 0.0)
-#  Then a.x = 4.3
-#    And a.y = -4.2
-#    And a.z = 3.1
-#    And a.w = 0.0
-#    And a is not a point
-#    And a is a vector
-#
-#Scenario: point() creates tuples with w=1
-#  Given p ← point(4, -4, 3)
-#  Then p = tuple(4, -4, 3, 1)
-#
-#Scenario: vector() creates tuples with w=0
-#  Given v ← vector(4, -4, 3)
-#  Then v = tuple(4, -4, 3, 0)
-#
+Scenario: A tuple with w=0 is a vector
+  Given a ← tuple(4.3, -4.2, 3.1, 0.0)
+  Then a.x = 4.3
+    And a.y = -4.2
+    And a.z = 3.1
+    And a.w = 0.0
+    And a is not a point
+    And a is a vector
+
+Scenario: point() creates tuples with w=1
+  Given p ← point(4, -4, 3)
+  Then p = tuple(4, -4, 3, 1)
+
+Scenario: vector() creates tuples with w=0
+  Given v ← vector(4, -4, 3)
+  Then v = tuple(4, -4, 3, 0)
+
 #Scenario: Adding two tuples
 #  Given a1 ← tuple(3, -2, 5, 1)
 #    And a2 ← tuple(-2, 3, 1, 0)
